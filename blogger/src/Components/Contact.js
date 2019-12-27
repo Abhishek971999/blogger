c9a58172b5d0b16e53406c461eaa2055cca4c5ff
Abhishek Mogaveera <abmogaveer971999@gmail.com>
2019-12-28 00:45:47 +0530

    contact component ui added

--- blogger/src/Components/Contact.js
+++ blogger/src/Components/Contact.js
@@ -2,8 +2,32 @@ import React, { Component } from 'react';
 class Contact extends Component {
   render() {
     return (
-      <div className='App'>
-        <h1>contact page bro</h1>
+      <div className='App contact-us-section'>
+        <div className='row justify-content-center'>
+          <div className='col-5'>
+            <form class='form-section'>
+              <div class='form-group'>
+                <h4 for='exampleInputEmail1' class='contact-header-text'>
+                  Enter your Email address
+                </h4>
+                <input
+                  type='email'
+                  class='form-control input-field'
+                  id='exampleInputEmail1'
+                  aria-describedby='emailHelp'
+                />
+                <small id='emailHelp' class='form-text'>
+                  We'll never share your email with anyone else.
+                </small>
+              </div>
+              <div className='row justify-content-center'>
+                <div className='col'>
+                  <button class='btn btn-sm btn-info'>Submit</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
